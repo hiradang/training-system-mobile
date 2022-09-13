9c83b58406910347711bf4b718d9ae95b83cc41c
hiradang <sunnybinhdang@gmail.com>
2022-09-13 12:32:51 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {View, Text, StyleSheet} from 'react-native';
+
+const App = () => {
+  return (
+    <View>
+      <Text>Test</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default App;
